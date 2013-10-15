c6347e2a7f451c03dec63b69eb021b309d7930af
Simon Williamsson <denialoffate@live.se>
2013-10-15 22:46:33 +0200

    Database changes

--- todinterface.sql
+++ todinterface.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Värd: localhost
--- Skapad: 14 okt 2013 kl 19:49
+-- Skapad: 15 okt 2013 kl 20:46
 -- Serverversion: 5.6.12-log
 -- PHP-version: 5.3.13
 
@@ -51,21 +51,26 @@ INSERT INTO `permissions` (`id`, `name`) VALUES
 
 CREATE TABLE IF NOT EXISTS `plugins` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(65) COLLATE utf8_bin NOT NULL,
+  `plugin_name` varchar(65) COLLATE utf8_bin NOT NULL,
+  `version` varchar(20) COLLATE utf8_bin NOT NULL,
+  `last_update` date NOT NULL,
   `link_download` varchar(256) COLLATE utf8_bin NOT NULL,
   `link_plugin` varchar(256) COLLATE utf8_bin NOT NULL,
-  `description` text COLLATE utf8_bin NOT NULL,
+  `plugin_description` text COLLATE utf8_bin NOT NULL,
   `active` tinyint(1) NOT NULL,
+  `broken` tinyint(1) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=3 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=10 ;
 
 --
 -- Dumpning av Data i tabell `plugins`
 --
 
-INSERT INTO `plugins` (`id`, `name`, `link_download`, `link_plugin`, `description`, `active`) VALUES
-(1, 'Test', 'http://google.se', 'http://google.se', '', 0),
-(2, 'test2', '', '', '', 0);
+INSERT INTO `plugins` (`id`, `plugin_name`, `version`, `last_update`, `link_download`, `link_plugin`, `plugin_description`, `active`, `broken`) VALUES
+(1, 'Torsten the World Turtle', '1.0', '2013-01-23', 'http://imgur.com/pF7owkL', '', 'Torsten!', 1, 0),
+(7, 'WorldEdit', '5.5.8', '2013-10-10', 'http://dev.bukkit.org/bukkit-plugins/worldedit/', 'http://wiki.sk89q.com/wiki/WorldEdit', 'Terraforming, protection', 1, 0),
+(8, 'WorldGuard', '5.8', '2013-10-13', 'http://dev.bukkit.org/bukkit-plugins/worldguard/', 'http://wiki.sk89q.com/wiki/WorldGuard', 'Protection and stuff', 1, 0),
+(9, 'Ion Cannon', '1.0.2', '2013-10-31', 'ioncannon.org', '', 'Blast users from SPAACE!', 0, 0);
 
 -- --------------------------------------------------------
 
@@ -90,26 +95,14 @@ CREATE TABLE IF NOT EXISTS `temples` (
   `costBravery` int(11) NOT NULL,
   `image` varchar(70) COLLATE utf8_bin NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=3 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=2 ;
 
 --
 -- Dumpning av Data i tabell `temples`
 --
 
 INSERT INTO `temples` (`id`, `name`, `entrancePosX`, `entrancePosY`, `entrancePosZ`, `description`, `other`, `is_finished`, `public`, `hasBravery`, `minBravery`, `maxBravery`, `rewardBravery`, `costBravery`, `image`) VALUES
-(1, 'Test Dungeon', 0, 0, 0, '<p>Test</p>', '<p>Test 2</p>\r\n<p>&nbsp;</p>\r\n<p>Test 3</p>', 0, 1, 0, 0, 0, 0, 0, 'S6w3c5rPtbJypZi3MBHa2s3HnEBg1rzvhptZLvZsyJC7kkPRBOU19kjX8Fc0poAi.jpg'),
-(2, 'Dark Lord''s Tower', -11232, 60, -11232, '<p>The natural home of all Dark Lords.</p>\r\n<p>They have gathered here for ages and will for all eternity.</p>\r\n<p>The tower looks on the outside to be really big, but for some reason it look a lot smaller on the inside...</p>', '', 1, 1, 1, 0, 0, 2, 0, '');
-
--- --------------------------------------------------------
-
---
--- Tabellstruktur `temples_plugins`
---
-
-CREATE TABLE IF NOT EXISTS `temples_plugins` (
-  `temple_id` int(11) NOT NULL,
-  `plugin_name` varchar(65) COLLATE utf8_bin NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+(1, 'Test', 551, 115, 512, '<p>Dungeon thing</p>', '<p>test</p>', 1, 1, 1, 12, 0, 122, 16, '8U4xFWjdFpYi3Nb0eLmO22rvxCmxpC0qnAqdWIj6I72NvuSsNKVtD5hpeZaCpU8W.jpg');
 
 -- --------------------------------------------------------
 
@@ -130,8 +123,7 @@ CREATE TABLE IF NOT EXISTS `temple_approvals` (
 --
 
 INSERT INTO `temple_approvals` (`temple_id`, `is_approved`, `approved_by`, `approved_date`) VALUES
-(1, 0, 0, '0000-00-00 00:00:00'),
-(2, 0, 0, '0000-00-00 00:00:00');
+(1, 0, 0, '0000-00-00 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -143,6 +135,14 @@ CREATE TABLE IF NOT EXISTS `temple_plugins` (
   `temple_id` int(11) NOT NULL,
   `plugin_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Dumpning av Data i tabell `temple_plugins`
+--
+
+INSERT INTO `temple_plugins` (`temple_id`, `plugin_id`) VALUES
+(1, 1),
+(1, 7);
 
 -- --------------------------------------------------------
 
